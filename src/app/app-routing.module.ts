330f7c9758a32457d468b2b9178ecf737d3d56bb
Jelle Vandevyvere <jelle.vandevyvere@student.hogent.be>
2020-05-04 11:44:08 +0200

    login and register works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,32 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './user/auth.guards';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CoinListComponent } from './coin/coin-list/coin-list.component';
+import { CoinDetailComponent } from './coin/coin-detail/coin-detail.component';
+import { CoinResolver } from './coin/CoinResolver';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
-
+/*
 const routes = [
   { path: 'coins/list', component: CoinListComponent },
+  {path: 'coins/detail/:id', component: CoinDetailComponent},
   //{ path: '', redirectTo: 'coin/list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
+];*/
+
+const routes: Routes = [
+  {
+    path: 'coin',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./coin/coin.module').then((mod) => mod.CoinModule),
+    data: { preload: false },
+  },
+  { path: 'coins/list',component: CoinListComponent },
+  {path: 'coins/detail/:id', component: CoinDetailComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
