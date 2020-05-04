1bdc9cfbf476589f16819daf0751ac886564f404
Jelle Vandevyvere <jelle.vandevyvere@student.hogent.be>
2020-05-04 14:05:38 +0200

    show portfolios from API

--- src/app/portfolio/all-portfolios/all-portfolios.component.ts
+++ src/app/portfolio/all-portfolios/all-portfolios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioDataService } from '../portfolio-data.service';
+import { Portfolio } from '../portfolio.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-all-portfolios',
@@ -6,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-portfolios.component.scss']
 })
 export class AllPortfoliosComponent implements OnInit {
+  private _fetchPortfolios$: Observable<Portfolio[]>= this._data.portfolios$;
   niewPortfolioMaken: boolean;
 
-  constructor() { }
+  constructor(private _data: PortfolioDataService) { }
 
   ngOnInit(): void {
   }
 
+
+  get portfolios$(): Observable<Portfolio[]> {
+    return this._fetchPortfolios$;
+  }
 
   nieuwPortfolioClicked(): void{
     this.niewPortfolioMaken=true;
---
+++ src/app/portfolio/portfolio-data.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Portfolio } from './portfolio.model';
+import { environment } from 'src/environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { shareReplay, tap, catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioDataService {
+
+  constructor(private http: HttpClient) { }
+
+  get portfolios$(): Observable<Portfolio[]> {
+    return this.http.get(`${environment.apiUrl}/portfolios/portfolios`).pipe(
+      tap(console.log),
+      shareReplay(1),
+      catchError(this.handleError),
+      map((list: any[]): Portfolio[] => list.map(Portfolio.fromJSON))
+    );
+  }
+
+  getportfolio$(name: string): Observable<Portfolio> {
+    return this.http
+      .get(`${environment.apiUrl}/portfolios/get=${name}`)
+      .pipe(catchError(this.handleError), map(Portfolio.fromJSON)); // returns just one recipe, as json
+  }
+
+  handleError(err: any): Observable<never> {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else if (err instanceof HttpErrorResponse) {
+      console.log(err);
+      errorMessage = `'${err.status} ${err.statusText}' when accessing '${err.url}'`;
+    } else {
+      errorMessage = err;
+    }
+    return throwError(errorMessage);
+  }
+}
